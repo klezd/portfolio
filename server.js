1cae4e90f4924f57994559f91c96ca5fdc4298a1
Quynh Dao <roxydao97@gmail.com>
2018-11-13 11:55:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -26,4 +26,4 @@ app.listen(app.get('port'), function() {
 var https = require('https')
 setInterval(function() {
     https.get("https://sienna-portfolio-197.herokuapp.com");
-}, 1785000);
+}, 1700000);
